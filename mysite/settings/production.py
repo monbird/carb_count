41b3e4245097cab0adcd28ec1c56106dfc987f01
Monika Ptaszynska <mon.ptasz@gmail.com>
2019-11-17 16:23:59 +0000

    prepare app to run on production server

---
+++ mysite/settings/production.py
@@ -0,0 +1,16 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['monbird.com', 'monptasz.com']
+
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'carb_count',
+         'USER': 'monbird',
+         'PASSWORD': '9_raaDI0_sM3Ll__1',
+         'HOST': 'localhost',
+         'PORT': '3306',
+     }
+ }
